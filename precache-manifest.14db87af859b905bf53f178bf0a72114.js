55eede81aabf84cef6be279885e6f250c0552ac8
xpchen-fa <likeasunshine@163.com>
2020-06-01 17:53:37 +0800

    Updates

---
+++ precache-manifest.14db87af859b905bf53f178bf0a72114.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6347072b17310b60ab5d7538352474c4",
+    "url": "/ts-react/index.html"
+  },
+  {
+    "revision": "bcc9133aa66131d70ddc",
+    "url": "/ts-react/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "98da6860ed11fa7ad82b",
+    "url": "/ts-react/static/css/main.023e0e1d.chunk.css"
+  },
+  {
+    "revision": "bcc9133aa66131d70ddc",
+    "url": "/ts-react/static/js/2.eec26372.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ts-react/static/js/2.eec26372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98da6860ed11fa7ad82b",
+    "url": "/ts-react/static/js/main.069dc81a.chunk.js"
+  },
+  {
+    "revision": "ff47af94ffff499639d1",
+    "url": "/ts-react/static/js/runtime-main.fcfb48f9.js"
+  },
+  {
+    "revision": "ac9049e18a97a7b7d4c6769233063fe6",
+    "url": "/ts-react/static/media/login-bg.ac9049e1.jpg"
+  }
+]);
